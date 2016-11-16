ac5737ae246e3b263344462bed9bab4f2b4cb672
Jorge Raze <jorge.raze@clevertech.biz>
2016-11-16 01:56:26 -0600

    WIP and reorganize

---
+++ r-programming/week4/rprog_data_ProgAssignment3-data/best.R
@@ -0,0 +1,50 @@
+# We need another function to avoid writting, write less is better
+getBestHospitalName <- function(col_num, state_sub) {
+    # Getting the desired set & supressing NA warnings instead of cleaning? Hum!
+    outcome_set <- suppressWarnings(as.numeric(state_sub[, col_num]))
+    # Getting  the min value
+    min <- min(outcome_set, na.rm=T)
+    # Getting an index for that value
+    min_index <- which(outcome_set == min)
+    # And getting the corresponding hospital name, sorting because ties
+    hospital_name <- sort(state_sub[min_index, 2])
+    # Return alphabeticaly the first
+    hospital_name[1]
+}
+best <- function(state, outcome) {
+    # Read the data
+    outcome_data <- read.csv("outcome-of-care-measures.csv", sep = ",", colClasses = "character")
+    # Allowed death reasons
+    valid_diseases <- c("heart attack", "heart failure", "pneumonia")
+    # Validation or die
+    if(!(state %in% outcome_data[,7])) {
+        stop("Invalid state name")
+    }
+    else {
+        # Validate or die
+        if(!(outcome %in% valid_diseases)) {
+            stop("Invalid outcome")
+        }
+        # State subset
+        state_sub <- outcome_data[outcome_data[,7]==state,]
+        if(outcome == valid_diseases[1]) {
+            name <- getBestHospitalName(11, state_sub)
+            return(name)
+        }
+        if(outcome == valid_diseases[2]) {
+            name <- getBestHospitalName(17, state_sub)
+            return(name)
+        }
+        if(outcome == valid_diseases[3]) {
+            name <- getBestHospitalName(23, state_sub)
+            return(name)
+        }
+    }
+}
+
+best("TX", "heart attack")
+best("TX", "heart failure")
+best("MD", "heart attack")
+best("MD", "pneumonia")
+best("BB", "heart attack")
+best("NY", "hert attack")
---
+++ r-programming/week4/rprog_data_ProgAssignment3-data/rankshospital.R
@@ -0,0 +1,64 @@
+# We need two helper classes for dealing with names and ordering
+# there might be a better way of doing this with apply() like functions
+getTopNHospitalRankNames <- function(outcome, state_sub, num) {
+    # Return the desired sorted value 
+    return(state_sub[, 2][order(outcome, state_sub[, 2])[num]])
+}
+getHospitalRank <- function(col_num, state_sub, num) {
+    # Getting the desired set & supressing NA warnings instead of cleaning? Hum!
+    outcome_set <- suppressWarnings(as.numeric(state_sub[, col_num]))
+    # Compute the length of the outcome_set
+    outcome_len <- dim(state_sub[!is.na(outcome_set), ])[1]
+    # Iy you want the "worst" then you get the last one
+    if(num == "worst") {
+        # Return "worst" hospital name with helper function
+        getTopNHospitalRankNames(outcome_set, state_sub, outcome_len)
+    }
+    else if (num > outcome_len) {
+        # Return NA if you have a higher "num" than the actual length
+        return(NA)
+    }
+    else {
+        # Or compute with the given value
+        getTopNHospitalRankNames(outcome_set, state_sub, num)
+    }
+    
+}
+rankhospital <- function(state, outcome, num = "best") {
+    # Read the data
+    outcome_data <- read.csv("outcome-of-care-measures.csv", sep = ",", colClasses = "character")
+    # Allowed death reasons
+    valid_diseases <- c("heart attack", "heart failure", "pneumonia")
+    # Validation or die
+    if(!(state %in% outcome_data[,7])) {
+        stop("Invalid state name")
+    }
+    else {
+        if(!(outcome %in% valid_diseases)) {
+            stop("Invalid outcome")
+        }
+        if(num == "best") {
+            best(state, 11)
+        }
+        # State subset
+        state_sub <- outcome_data[outcome_data[,7]==state,]
+        if(outcome == valid_diseases[1]) {
+            name <- getHospitalRank(11, state_sub, num)
+            return(name)
+        }
+        if(outcome == valid_diseases[2]) {
+            name <- getHospitalRank(17, state_sub, num)
+            return(name)
+        }
+        if(outcome == valid_diseases[3]) {
+            name <- getHospitalRank(23, state_sub, num)
+            return(name)
+        }
+    }
+    
+}
+
+# tests
+rankhospital("TX", "heart failure", 4)
+rankhospital("MD", "heart attack", "worst")
+rankhospital("MN", "heart attack", 5000)
