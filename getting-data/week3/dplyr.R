8dff2a23016197fa8cac8eaa05a71a477e5d9639
Jorge Raze <jorge.raze@clevertech.biz>
2016-12-11 09:11:30 -0600

    More notes

---
+++ getting-data/week3/dplyr.R
@@ -0,0 +1,62 @@
+# Managing Data Frames with dplyr
+# dplyr Verbs
+# select: return a subset of the columns of a data frame
+# filter: extract a subset of rows from a data frame based on
+# logical conditions
+# arrange: reorder rows of a data frame
+# rename: rename variables in a data frame
+# mutate: add new variables/columns or transform existing variables
+# summari ze/se: generate summary statistics of different variables
+# in the data frame, possibly within strate
+# There's also a handy print method that prevents you from
+# printing a lot of data to the console.
+#
+# dplyr Properties
+# The first argument is a data frame.
+# The subsequent arguments describe that to do with it,
+# and you can refer to columns in the data frame directly
+# without using the $ operator (just use the names)
+# The result is a new data frame
+# Data frames must be properly formatted and annotated
+# for this to all be usefull.
+#
+# Using the tools
+library(dplyr)
+options(width = 105)
+chicago <- readRDS("chicago.rds")
+dim(chicago)
+str(chicago)
+names(chicago)
+# Select all the columns from city to dptp
+head(select(chicago, city:dptp))
+# Select all the columns except for those you remove using -
+head(select(chicago, -(city:dptp)))
+# Doing the same with regular R functions
+i <- match("city", names(chicago))
+j <- match("dptp", names(chicago))
+head(chicago[, -(i:j)])
+# The filter function for subsetting based on particular conditions
+chic.f <- filter(chicago, pm25tmean2 > 30)
+head(chic.f, 10)
+# You can also use several logical conditions
+# For example, not just pm25tmean2 greater than 30
+# but also tmpd greater than 80
+chic.f <- filter(chicago, pm25tmean2 > 30 & tmpd > 80)
+head(chic.f)
+# Now let's arrange the data frame with the arrange command
+# and using a certain variable, in this case the date.
+chicago <- arrange(chicago, date)
+head(chicago)
+tail(chicago)
+# And here's how to do it in a descending way
+chicago <- arrange(chicago, desc(date))
+# Rename
+# So let's rename variables by using the command rename
+chicago <- rename(chicago, pm25 = pm25tmean2, dewpoint = dptp)
+head(chicago)
+# Mutate
+# The mutate command is used to create new variables
+# in this case, let's create a new variables called pm25detrend
+# which is the pm25 variable minus the mean.
+chicago <- mutate(chicago, pm25detrend = pm25-mean(pm25, na.rm = TRUE))
+head(select(chicago, pm25, pm25detrend))
--- getting-data/week3/reshaping_data.R
+++ getting-data/week3/reshaping_data.R
@@ -3,3 +3,48 @@
 # 1. Each variable forms one column
 # 2. Each observation forms a row
 # 3. Each table/file stores data about one kind of observation (e.g. people, hospitals)
+library(reshape2)
+head(mtcars)
+# Melting data
+mtcars$carname <- rownames(mtcars)
+# To the data frame we're going to melt
+carMelt <- melt(mtcars, id = c("carname", "gear", "cyl"), measure.vars = c("mpg", "hp"))
+# A bunch of id values carname, gear, cyl
+# one row for every mpg and one row for each hp
+head(carMelt, n = 3)
+tail(carMelt, n = 3)
+# Casting data frames
+# With the dcast function we can recast the data frame
+# into particular shapes and particular data frames
+# We want to see cyl variable broken down by different vairables
+cylData <- dcast(carMelt, cyl ~ variable)
+# Summarizes the data
+cylData
+# Another way of summarize
+cylData <- dcast(carMelt, cyl ~ variable, mean)
+cylData
+# Averaging values
+head(InsectSprays)
+# The sum of the count for each of the different sprays
+# Apply to count along of different spray the sum function
+tapply(InsectSprays$count, InsectSprays$spray, sum)
+# Another way - split
+spIns <- split(InsectSprays$count, InsectSprays$spray)
+spIns
+# and also apply
+sprCount <- lapply(spIns, sum)
+sprCount
+# Another way - combine
+unlist(sprCount)
+sapply(spIns, sum)
+# Another way - plyr package
+library(plyr)
+ddply(InsectSprays, .(spray), summarize, sum = sum(count))
+# Creating a new variable
+spraySums <- ddply(InsectSprays, .(spray), summarize, sum=ave(count, FUN=sum))
+dim(spraySums)
+head(spraySums)
+# Another functions to look at
+# acast - for casting as multi-dimensional arrays
+# arrange - for faster reordering without using order() commands
+# mutate - adding new variables
